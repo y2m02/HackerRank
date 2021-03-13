bf9744cfb0e33f4de42fd2e63b319becb1592c17
raimyy2m <raimymejia@gmail.com>
2021-03-13 15:34:01 -0400

    Sum min max

---
+++ HackerRank/MinMaxSum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace MinMaxSum
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CalculateMinAndMaxSum(
+                new long[]
+                {
+                    140537896, 243908675, 670291834, 923018467, 520718469,
+                }
+            );
+
+            Console.ReadLine();
+        }
+
+        private static void CalculateMinAndMaxSum(long[] numbers)
+        {
+            var sortedNumbers = numbers.OrderBy(n => n).ToList();
+
+            var minSum = sortedNumbers[0] +
+                         sortedNumbers[1] +
+                         sortedNumbers[2] +
+                         sortedNumbers[3];
+
+            var maxSum = sortedNumbers[1] +
+                         sortedNumbers[2] +
+                         sortedNumbers[3] +
+                         sortedNumbers[4];
+
+            Console.Write($"{minSum} {maxSum}");
+        }
+    }
+}
