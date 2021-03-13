46a49ccb7daab33fe125b0a4e1167580a94ce3d5
raimyy2m <raimymejia@gmail.com>
2021-03-13 15:13:15 -0400

    Not done

---
+++ HackerRank/Staircase/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Staircase
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Staircase(6);
+
+            Console.ReadKey();
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void Staircase(int n)
+        {
+            for (int symbolsCounter = 1, whiteSpacesCounter = n;
+                 symbolsCounter <= n;
+                 symbolsCounter++, whiteSpacesCounter--
+            )
+            {
+                for (var counter = 1; counter <= whiteSpacesCounter; counter++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (var counter = 1; counter <= symbolsCounter; counter++)
+                {
+                    Console.Write("#");
+                }
+
+                if (symbolsCounter == n)
+                {
+                    return;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
