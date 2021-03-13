286d8007b942ce0612f805f31ddf5d7293d59b75
raimyy2m <raimymejia@gmail.com>
2021-03-13 15:41:35 -0400

    birthday cake candles

---
+++ HackerRank/BirthdayCakeCandles/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BirthdayCakeCandles
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var a = CountTallestCandlesTotal(
+                new List<int>
+                {
+                    4, 4, 1, 3,
+                }
+            );
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static int CountTallestCandlesTotal(List<int> candles)
+        {
+            var tallestCandle = candles.Max();
+
+            return candles.Count(x => x == tallestCandle);
+        }
+    }
+}
