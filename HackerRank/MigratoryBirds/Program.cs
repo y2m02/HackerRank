5d39df21c357b1d2ff938ab645f05ff5b6e714ec
raimyy2m <raimymejia@gmail.com>
2021-03-13 20:56:56 -0400

    Migratory birds

---
+++ HackerRank/MigratoryBirds/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MigratoryBirds
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var a = GetTheSmallestBirdId(
+                new List<int>
+                {
+                    1,
+                    1,
+                    1,
+                    4,
+                    4,
+                    4,
+                    4,
+                    4,
+                    5,
+                    5,
+                    5,
+                    5,
+                    5,
+                    5,
+                    3,
+                    3,
+                    3,
+                    3,
+                    3,
+                    3,
+                }
+            );
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static int GetTheSmallestBirdId(List<int> birdIds)
+        {
+            return birdIds
+                .GroupBy(id => id)
+                .Select(group => (id: group.Key, repetitions: group.Count()))
+                .GroupBy(bird => bird.repetitions)
+                .OrderByDescending(group => group.Key)
+                .First()
+                .OrderBy(bird => bird.id)
+                .First()
+                .id;
+        }
+    }
+}
