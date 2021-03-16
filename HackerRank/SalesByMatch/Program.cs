1cc9480e69302f1ae35e0ec667e57020d808ea20
raimyy2m <raimymejia@gmail.com>
2021-03-15 23:57:34 -0400

    Sales by match

---
+++ HackerRank/SalesByMatch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace SalesByMatch
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var a = GetPairsTotal(
+                9,
+                new[]
+                {
+                    10, 20, 20, 10, 10, 30, 50, 10, 20,
+                }
+            );
+
+            Console.ReadKey();
+        }
+
+        private static int GetPairsTotal(int n, int[] ar)
+        {
+            var container = new int[ar.Max() + 1];
+
+            foreach (var number in ar)
+            {
+                container[number]++;
+            }
+
+            return container.Sum(w => w / 2);
+        }
+    }
+}
