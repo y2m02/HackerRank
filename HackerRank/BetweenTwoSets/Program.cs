1424b003b616411bf410aa39c5ee9780f7af8169
raimyy2m <raimymejia@gmail.com>
2021-03-16 00:33:24 -0400

    Between 2 sets

---
+++ HackerRank/BetweenTwoSets/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BetweenTwoSets
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var a = GetTotalFactors(
+                new List<int>
+                {
+                    2, 4,
+                },
+                new List<int>
+                {
+                    16, 32, 96,
+                }
+            );
+
+            Console.ReadKey();
+        }
+
+        private static int GetTotalFactors(List<int> a, List<int> b)
+        {
+            return Enumerable
+                .Range(a.Max(), b.Min())
+                .Count(
+                    w => a.TrueForAll(x => w % x == 0) &&
+                         b.TrueForAll(x => x % w == 0)
+                );
+        }
+    }
+}
