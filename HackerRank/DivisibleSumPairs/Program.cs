168af4d9fa8c150a6a50fc658b3070c125a5c644
raimyy2m <raimymejia@gmail.com>
2021-03-15 22:58:45 -0400

    Divisible sum pair

---
+++ HackerRank/DivisibleSumPairs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DivisibleSumPairs
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var a = DivisibleSumPairs(
+                6,
+                3,
+                new[]
+                {
+                    1, 3, 2, 6, 1, 2,
+                }
+            );
+
+            Console.ReadKey();
+        }
+
+        private static int DivisibleSumPairs(int n, int k, int[] ar)
+        {
+            var counter = 0;
+
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = i + 1; j < n; j++)
+                {
+                    if ((ar[i] + ar[j]) % k == 0)
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            return counter;
+        }
+    }
+}
