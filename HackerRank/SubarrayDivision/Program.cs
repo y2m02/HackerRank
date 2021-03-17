f4c5f2be1f87c157fde4ce3f3c5c04a3ac670a39
raimyy2m <raimymejia@gmail.com>
2021-03-16 20:02:15 -0400

    Sub Array division

---
+++ HackerRank/SubarrayDivision/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace SubarrayDivision
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var a = GetChocolateDivisions(
+                new List<int>
+                {
+                    1,
+                    2,
+                    1,
+                    3,
+                    2,
+                },
+                3,
+                2
+            );
+
+            Console.ReadKey();
+        }
+
+        private static int GetChocolateDivisions(List<int> s, int d, int m)
+        {
+            var counter = 0;
+
+            // With linq
+            for (var i = 0; i <= s.Count - m; i++)
+            {
+                if (s.Skip(i).Take(m).Sum(w => w) == d)
+                {
+                    counter++;
+                }
+            }
+
+            // Without linq
+            //for (var i = 0; i <= s.Count - m; i++)
+            //{
+            //    var sum = 0;
+
+            //    for (var j = 0; j < m; j++)
+            //    {
+            //        sum += s[i + j];
+            //    }
+
+            //    if (sum == d)
+            //    {
+            //        counter++;
+            //    }
+            //}
+
+            return counter;
+        }
+    }
+}
