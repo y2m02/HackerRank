e6399687bf244cf5383e8d1d950a8a777c52916f
raimyy2m <raimymejia@gmail.com>
2021-03-18 22:21:01 -0400

    Cats and mouse

---
+++ HackerRank/CatsAndAMouse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CatsAndAMouse
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var a = GetTheClosestCat(2, 5, 4);
+            Console.ReadKey();
+        }
+
+        private static string GetTheClosestCat(int x, int y, int z)
+        {
+            var catADistance = Math.Abs(z - x);
+            var catBDistance = Math.Abs(z - y);
+
+            return catADistance == catBDistance
+                ? "Mouse C"
+                : catADistance < catBDistance
+                    ? "Cat A"
+                    : "Cat B";
+        }
+    }
+}
