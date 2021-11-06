0c49ee83b5b5cef51cc1cf3ac81f2a554abc3ea6
raimyy2m <raimymejia@gmail.com>
2021-11-06 13:44:50 -0400

    Counting valleys

---
+++ HackerRank/CountingValleys/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace CountingValleys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = CountingValleys(8, "UDDDUDUU");
+
+            Console.ReadKey();
+        }
+
+        public static int CountingValleys(int steps, string path)
+        {
+            var max = 0;
+            var counter = 0;
+            var backAtTheSeaLevel=true;
+
+            foreach (var @char in path)
+            {
+                counter += @char == 'D' ? -1 : 1;
+
+                if (counter == 0)
+                {
+                    backAtTheSeaLevel = true;
+                }
+
+                if (backAtTheSeaLevel && counter < 0)
+                {
+                    max++;
+                    backAtTheSeaLevel = false;
+                }
+            }
+
+            return max;
+        }
+    }
+}
